76c0ba4bd09e925352e97cba6d03f8f32cb0c6db
github <mmoloshova@gmail.com>
2023-01-24 21:06:26 +0300

    f

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,17 @@ public class Main {
         BusDriver busDriver = new BusDriver("Arslan", "bus driver", 30);
         BusDriver busDriver2 = new BusDriver("Mirbek", "bus driver", 25);
         Car car = new Car(2345, 2022, "black", 777,taxiDriver ,busDriver2);
+        car.run();
+        car.stop();
         Bus bus = new Bus(2234, 2014, "white", 555,busDriver,shopAsistant);
+        bus.run();
+        bus.stop();
         Trolleybus trolleybus = new Trolleybus(5789, 2019, "white", 989,shopAsistant);
-        System.out.println(car);
+        trolleybus.run();
+        trolleybus.stop();
         System.out.println(bus);
         System.out.println(trolleybus);
+        System.out.println(car);
 
 
     }
